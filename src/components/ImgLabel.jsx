4d5f502ee8a462bd9609df442e317df73f994005
zoeGuava <zoeguava@gmail.com>
2022-04-09 23:48:23 +0800

    feat: 🎸 delete the file didn't use

--- src/components/ImgLabel.jsx
+++ src/components/ImgLabel.jsx
@@ -1,9 +1,17 @@
-function ImgLabel({ getImg, imgLink }) {
+function ImgLabel({ getImg, imgLinks }) {
+  console.log('ImgLabel imgLinks: ', imgLinks);
   return (
     <>
       <h1>get image component is here</h1>
+      <p>{imgLinks}</p>
       <button onClick={getImg}>get img</button>
-      {!!imgLink ? <img src={imgLink} alt="" /> : <p>imgLink is empty</p>}
+      {!!imgLinks ? (
+        imgLinks.map((link, index) => {
+          return <img src={link} alt="" key={`album_${index}`} />;
+        })
+      ) : (
+        <p>imgLinks is empty</p>
+      )}
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,25 +5,38 @@ import ImgLabel from '../components/ImgLabel';
 import axios from 'axios';
 
 function HomePage() {
-  const imgId = 'INesDmY';
-  const [imgLink, setImgLink] = useState('');
+  // const imgId = 'INesDmY';
+  const albumId = 'ABDhZzE';
+  const [imgLinks, setImgLinks] = useState([]);
 
   const apiOptions = (id, method) => {
     const url = imgurApi(id)[method]();
     const options = {
       headers: {
-        Authorization: `Client-ID ${CLIENT_ID}`
-      }
+        Authorization: `Client-ID ${CLIENT_ID}`,
+      },
     };
-    return {url, options};
-  }
+    return { url, options };
+  };
 
-  async function getImg(id) {
+  // async function getImg(id) {
+  //   try {
+  //     const { url, options } = apiOptions(id, 'getImgApi');
+  //     const res = await axios.get(url, options);
+  //     const link = res.data.data.link;
+  //     setImgLinks(link);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  async function getAlbum(id) {
     try {
-      const {url, options} = apiOptions(id, 'getImgApi');
+      const { url, options } = apiOptions(id, 'getAlbumApi');
       const res = await axios.get(url, options);
-      const link = res.data.data.link;
-      setImgLink(link); 
+      const images = res.data.data.images;
+      const links = images.map(item => item.link);
+      setImgLinks(links);
     } catch (error) {
       console.log(error);
     }
@@ -32,10 +45,8 @@ function HomePage() {
   return (
     <>
       <h1>HomePage is working</h1>
-      <ImgLabel
-        getImg={() => getImg(imgId)}
-        imgLink={imgLink}
-      />
+      {/* <ImgLabel getImg={() => getImg(imgId)} imgLinks={imgLinks} /> */}
+      <ImgLabel getImg={() => getAlbum(albumId)} imgLinks={imgLinks} />
     </>
   );
 }
